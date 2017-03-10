57ff3551bffd1c97769743e5460a679e09dbc6dc
ohmyxm <wmx2112@gmail.com>
2017-03-10 23:57:17 +0800

    改用pem

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cert-gen
+
 Certification generator
+
+### Usage
+
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const pem = require('pem')
+// const {} = require('./utils.js')
+
+exports.genRootCert = function() {
+  // body...
+}
+
+exports.genCertByHostname = function() {
+  // body...
+}
