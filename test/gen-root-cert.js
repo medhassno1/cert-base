37e7c947f03dbccdf970593eea6ecd3e3af648a2
ohmyxm <wmx2112@gmail.com>
2017-03-14 01:08:46 +0800

    用tape来测试

--- test/gen-root-cert.js
+++ test/gen-root-cert.js
@@ -1,3 +1,4 @@
+const test = require('tape')
 // const CertGenerator = require('../lib/index.js')
 
 // const cg = new CertGenerator({
@@ -8,3 +9,12 @@
 //   commonName: 'my-common-name'
 // })
 
+test('example test', function(t) {
+  t.plan(1)
+
+  t.equal(typeof Date.now, 'function')
+  // const start = Date.now()
+  // setTimeout(function () {
+  //   t.equal(Date.now() - start, 104)
+  // }, 100)
+})
